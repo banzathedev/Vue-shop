0679247ff270e3560c4b4300237fe1cbe2e262f9
Giovanni Raposo Pinheiro <g.raposo@outlook.com.br>
2021-06-22 12:02:32 -0300

    commit

--- vue-shop-complete/src/router/index.js
+++ vue-shop-complete/src/router/index.js
@@ -1,6 +1,9 @@
 import Vue from 'vue';
 import VueRouter from 'vue-router';
 import Home from '../views/Home.vue';
+import Admin from '../views/Admin.vue';
+import Overview from '../views/Overview.vue'
+import Products from '../views/Products.vue'
 
 Vue.use(VueRouter);
 
@@ -9,6 +12,23 @@ const routes = [
     path: '/',
     name: 'home',
     component: Home,
+  },
+  {
+    path: '/admin',
+    name: 'admin',
+    component: Admin,
+    children: [
+      {
+        path: 'overview',
+        name: 'overview',
+        component: Overview,
+      },
+      {
+        path: 'products',
+        name: 'products',
+        component: Products,
+      },
+    ]
   },
   {
     path: '/about',
