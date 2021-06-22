4b77ebd9470d3586bc48d9bad724917f94dbe13d
Giovanni Raposo Pinheiro <g.raposo@outlook.com.br>
2021-06-22 09:23:21 -0300

    commit

---
+++ vue-shop-complete/src/main.js
@@ -0,0 +1,20 @@
+import Vue from 'vue';
+import App from './App.vue';
+import router from './router';
+import store from './store';
+import 'bootstrap';
+import Jquery from 'jquery';
+
+window.$ = window.Jquery = jQuery;
+
+import 'popper.js';
+import './assets/app.scss';
+
+
+Vue.config.productionTip = false;
+
+new Vue({
+  router,
+  store,
+  render: (h) => h(App),
+}).$mount('#app');
